e85dab76a8403c170cb2ebd153bf995e09e7c709
Ilyich Vismara <ilyich@arkemis.it>
2025-03-30 21:40:14 +0200

    fix: add github links

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -2,6 +2,7 @@ import { Logo } from "@/components/logo";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export const baseOptions: BaseLayoutProps = {
+	githubUrl: "https://github.com/arkemis-labs/puedo",
 	nav: {
 		title: (
 			<>
--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
@@ -7,10 +8,16 @@ const inter = Inter({
 	subsets: ["latin"],
 });
 
+export const metadata: Metadata = {
+	title: "¿puedo?",
+	description: "Deal with access control in a simple way.",
+	metadataBase: new URL("https://puedo.arkehub.com"),
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
-			<body className="flex flex-col min-h-screen">
+			<body className="flex flex-col min-h-dvh">
 				<RootProvider>{children}</RootProvider>
 			</body>
 		</html>
